1385b61cdf9c066278c1c0ef4ec5bacb8bf02913
David Weinman <david@weinman.com>
2014-09-30 18:10:21 -0700

    updated website
    error catching and better labeling on return amount

--- wheres_my_refund_site/Default.aspx.cs
+++ wheres_my_refund_site/Default.aspx.cs
@@ -32,35 +32,58 @@ public partial class _Default : System.Web.UI.Page
         {
             lblErrorMessage.Text = "Missing SSN.<br><br />";
             lblErrorMessage.Visible = true;
+			return;
         }
         else if (statusInput.Text == "")
         {
             lblErrorMessage.Text = "Missing Filing Status.<br><br />";
             lblErrorMessage.Visible = true;
+			return;
         }
         else if (amtInput.Text == "")
         {
             lblErrorMessage.Text = "Missing Refund Amount.<br><br />";
             lblErrorMessage.Visible = true;
+			return;
         }
         else
         {
             conn1 = "SELECT     TaxID, Salutation, Suffix, LastName, FirstName, SSN     FROM CitizenData     WHERE (SSN = '" + socialInput.Text + "')";
             getReturn();
         }
+		return;
     }
+
+	private void clearLabels() 
+	{
+		rfndSSNLabel.Text = "";
+		rfndFNameLabel.Text = "";
+		rfndLNameLabel.Text = "";
+		rfndSufxLabel.Text = "";
+		rfndSalutLabel.Text = "";
+		taxID = "";
+		rfndDateResolvedLabel.Text = "";
+		rfndAMTLabel.Text = "";
+		rfndFilingStatusLabel.Text = "";
+		dateFiledLabel.Text = "";
+	}
 
     private void getReturn()
     {
         SqlCommand cmd = new SqlCommand(conn1, objConn);
         SqlDataReader rdr;
+		bool hasrows = false;
         objConn.Open();
         rdr = cmd.ExecuteReader();
 
         /* get labels from CitizenData! */
-        rdr.Read();
-        /* put a condition here to catch a 'no results found from query'! */ 
-		rfndSSNLabel.Text = String.Format("{0}", rdr[5]);
+        hasrows = rdr.Read();
+		if ( !hasrows ) {
+			lblErrorMessage.Text = "Invalid SSN.<br><br />";
+            lblErrorMessage.Visible = true;
+			return;
+		}
+		rfndSSNLabel.Text = String.Format("<strong>SSN:</strong> {0}", rdr[5]);
         rfndFNameLabel.Text = String.Format("{0}", rdr[4]);
         rfndLNameLabel.Text = String.Format("{0}", rdr[3]);
         rfndSufxLabel.Text = String.Format("{0}", rdr[2]);
@@ -74,20 +97,26 @@ public partial class _Default : System.Web.UI.Page
         cmd = new SqlCommand(conn2, objConn);
         objConn.Open();
         rdr = cmd.ExecuteReader();
-        rdr.Read();
-        rfndDateResolvedLabel.Text = String.Format("{0}", rdr[0]);
+        hasrows = rdr.Read();
+        rfndDateResolvedLabel.Text = String.Format("<strong>Date Resolved:</strong> {0}", rdr[0]);
         rdr.Close();
 		objConn.Close();
 
         /* and labels from TaxReturnInformation! */
-        conn3 = "SELECT     DateFiled, FilingStatus, ReturnAmount     FROM TaxReturnInformation     WHERE (TaxID = '" + taxID + "') AND (FilingStatus = '" + statusInput.Text + "')";
+        conn3 = "SELECT     DateFiled, FilingStatus, ReturnAmount     FROM TaxReturnInformation     WHERE (ReturnAmount = '" + amtInput.Text + "') AND (FilingStatus = '" + statusInput.Text + "')";
 		cmd = new SqlCommand(conn3, objConn);
         objConn.Open();
         rdr = cmd.ExecuteReader();
-        rdr.Read();
-		rfndAMTLabel.Text = String.Format("{0}", rdr[2]);
-		rfndFilingStatusLabel.Text = String.Format("{0}", rdr[1]);
-        dateFiledLabel.Text = String.Format("{0}", rdr[0]);
+        hasrows = rdr.Read();
+		if ( !hasrows ) {
+			clearLabels();
+			lblErrorMessage.Text = "Invalid Filing Status or Return Amount.<br><br />";
+            lblErrorMessage.Visible = true;
+			return;
+		}
+		rfndAMTLabel.Text = String.Format("<strong>Amount Due:</strong> ${0}", rdr[2]);
+		rfndFilingStatusLabel.Text = String.Format("<strong>Filing Status:</strong> {0}", rdr[1]);
+        dateFiledLabel.Text = String.Format("<strong>Date Filed:</strong> {0}", rdr[0]);
         rdr.Close();
 		objConn.Close();
 
